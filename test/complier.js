233289f19c3314fb4d7291d04bd5fe2c47057fd7
zhaopeng <719197352@qq.com>
2019-05-21 15:05:27 +0800

    fix: rm unused code

--- test/complier.js
+++ test/complier.js
@@ -1,29 +1,6 @@
 import webpack from 'webpack'
 import memoryfs from 'memory-fs'
 
-// {
-//     entry: './index.js',
-//     mode: 'development',
-//     module: {
-//         rules: [{
-//             test: /\.js$/,
-//             use: 'babel-loader',
-//             exclude: /node_modules/
-//         }, {
-//             test: /\.mst$/,
-//             use: [
-//                 'babel-loader',
-//                 {
-//                     loader: resolve(__dirname, '../index.js'),
-//                     options: {
-//                         data,
-//                     }
-//                 }
-//             ],
-//         }]
-//     }
-// }
-
 export default function(config) {
     const complier = webpack(config)
 
